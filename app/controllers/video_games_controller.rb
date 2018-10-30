8a3a19e8ed5720015a71381584b3ba074733e15b
Pignataro67 <Pignataro67@gmail.com>
2018-10-30 11:38:13 -0700

    adding file with private, #v_g_p and #s_v_g

--- app/controllers/video_games_controller.rb
+++ app/controllers/video_games_controller.rb
@@ -15,4 +15,13 @@ class VideoGamesController < ApplicationController
     if @video_game.update(video_game_params)
     render json: @video_game
   end
+
+  private
+  def video_game_params
+    params.permit(:name, :age_range, :description, :pic_url)
+  end
+
+  def set_video_game
+    @video_game = Video_game.find_by(id: params[:id])
+  end
 end
