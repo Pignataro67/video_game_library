32cc6d01d3eea66b3cb5f7577319b7da7d220dbb
Pignataro67 <Pignataro67@gmail.com>
2018-11-06 11:17:35 -0800

    adding file with a f to add a vg

--- client/src/actions/videoGameActions.js
+++ client/src/actions/videoGameActions.js
@@ -9,10 +9,21 @@ const getVideoGames = videoGames => {
   }
 }
 
+const addVideoGame = videoGame => {
+  return {
+    type: 'ADD_VIDEO_GAME',
+    videoGame
+  }
+}
+
 export const fetchVideoGames = () => {
   return dispatch => {
     return fetch(url)
       .then(res => res.json())
       .then(videoGames => dispatch(getVideoGames(videoGames)))
   }
+}
+
+export const createVideoGame = videoGame => {
+  
 }
