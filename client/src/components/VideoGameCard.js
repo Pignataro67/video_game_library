369eb55e6bb6d802e5a2a92b177715944773fea3
Pignataro67 <Pignataro67@gmail.com>
2018-11-07 09:17:48 -0800

    adding file with better display/rendering of data

--- client/src/components/VideoGameCard.js
+++ client/src/components/VideoGameCard.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 const VideoGameCard = ({ videoGame }) =>
  <div key={videoGame.id} className='container text-center'>
-   <p>Video Game Name: {videoGame.name}</p>
+  <Link to={`/videoGames/${videoGame.id}`}>{videoGame}</Link>
+   {/* <p>Video Game Name: {videoGame.name}</p> */}
    <p>Age Range: {videoGame.age_range}</p>
-   <p>Description: </p>
-   <img src={`${videoGame.pic_url}`} style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
+   <p>Description: {videoGame.description}</p>
+   {videoGame.pic_url? (
+     <img src={`${videoGame.pic_url}`} style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
+   ) : (
+     <img src=''
+     style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
+   )
+  }
  </div>
 
 export default VideoGameCard
