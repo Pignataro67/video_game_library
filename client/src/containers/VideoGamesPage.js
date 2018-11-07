e126e135540970822465374be83e8c4b794ecf45
Pignataro67 <Pignataro67@gmail.com>
2018-11-07 09:26:17 -0800

    adding file with imports, cwm func., and rendering of vg list

---
+++ client/src/containers/VideoGamesPage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as actions from '../actions/videoGameActions'
+// import VideoGameCard from '../components/VideoGameCard'
+import VideoGamesList from '../components/VideoGamesList'
+import VideoGameShow from '../containers/VideoGameShow'
+
+class VideoGamesPage extends Component {
+  componentWillMount() {
+    const { actions } = this.props
+    actions.fetchVideoGames()
+  }
+
+  render() {
+    
+    const { videoGames, match } = this.props
+    
+    return(
+      <div>
+        <VideoGamesList videoGames={videoGames} />
+        <Route path={`${match.url}/:videoGameId`} component={VideoGameShow} />
+      </div>
+      )
+    }
+  }
