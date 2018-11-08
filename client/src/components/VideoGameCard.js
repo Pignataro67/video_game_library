1e86ec28d816aecc920a69ca6f307df521747939
Pignataro67 <Pignataro67@gmail.com>
2018-11-08 06:05:24 -0800

    working on improved functionality

--- client/src/components/VideoGameCard.js
+++ client/src/components/VideoGameCard.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const VideoGameCard = ({ videoGame }) =>
+  <div><br />
  <div key={videoGame.id} className='container text-center'>
-  <Link to={`/videoGames/${videoGame.id}`}>{videoGame}</Link>
-   <p>Age Range: {videoGame.age_range}</p>
+  <Link to={`/videoGames/${videoGame.id}`}>{videoGame}</Link><br />
+   {/* <p>Age Range: {videoGame.age_range}</p>
    <p>Description: {videoGame.description}</p>
+   <Link to={`/videoGames/${videoGame.id}`}>{videoGame.name}</Link><br /> */}
    {videoGame.pic_url? (
      <img src={`${videoGame.pic_url}`} style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
    ) : (
@@ -13,6 +15,7 @@ const VideoGameCard = ({ videoGame }) =>
      style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
    )
   }
+ </div><br />
  </div>
 
 export default VideoGameCard
