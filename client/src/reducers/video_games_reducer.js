3cc8561f7fcc518096580b46be0fdcba1a0b2ef4
Pignataro67 <Pignataro67@gmail.com>
2018-11-08 11:50:26 -0800

    adding a vgf case in the switch and uvg case

--- client/src/reducers/video_games_reducer.js
+++ client/src/reducers/video_games_reducer.js
@@ -2,8 +2,24 @@ export default (state = [], action) => {
     switch (action.type) {
         case 'FETCH_VIDEO_GAMES':
           return action.videoGames
+        case 'VIDEO_GAME_FETCHED':
+          const index = state.findIndex(videoGame => videoGame.id === action.videoGame.id)
+          if (index > -1) {
+            return state.map(videoGame => {
+              if (videoGame.id === action.videoGame.id) {
+                return action.videoGame
+              } else {
+                return videoGame
+              }
+            })
+          } else {
+            return [...state, action.videoGame]
+          }
+          
         case 'ADD_VIDEO_GAME':
           return [...state, action.videoGame]
+        case 'UPDATE_VIDEO_GAME':
+          return [...state.filter(videoGame => videoGame.id !== action.videoGame.id), action.videoGame]  
         case 'DELETE_TOY':
           return state.filter(videoGame => videoGame.id !== action.id)
         default:
