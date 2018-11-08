61298d11a0f279a3c1aedbbe2df67df793fa963d
Pignataro67 <Pignataro67@gmail.com>
2018-11-08 05:59:11 -0800

    adding file with big. pic h and w and inc. link func.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+// import VideoGamesPage from '../containers/VideoGamesPage'
 
 const Home = () =>
   <div>  
@@ -8,10 +9,12 @@ const Home = () =>
       <h1>Welcome to the Video Game Library</h1>
       <p>This is the place to keep track of your Video Games, from the NES days to Playstation/XBOX</p>
       <img src="https://i.pinimg.com/originals/f6/17/92/f61792692722c6db3521d931d3f6adb4.jpg"
-      style={{height: '250px', width: '200px', margin: 'auto'}}
+      style={{height: '800px', width: '1800px', margin: 'auto'}}
       alt='missing' />
       <br></br>
-      <Link to='/videoGames'>View All Video Games</Link>
+      <Link to='/videoGames'>Click to View All Video Games</Link><br></br>
+      <Link to='/videoGames/new'>Click to Begin Adding Video Games</Link>
+      <br></br>
     </div>
   </div>
 
