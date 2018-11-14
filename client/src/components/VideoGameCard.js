45204732640d30a49f028e3a3502d31ae542b97d
Pignataro67 <Pignataro67@gmail.com>
2018-11-13 19:35:27 -0800

    adding file with nav-link className

--- client/src/components/VideoGameCard.js
+++ client/src/components/VideoGameCard.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-const VideoGameCard = ({ videoGame }) =>
-  <div><br />
- <div key={videoGame.id} className='container text-center'>
-  <Link to={`/videoGames/${videoGame.id}`}>{videoGame}</Link><br />
-   {/* <p>Age Range: {videoGame.age_range}</p>
-   <p>Description: {videoGame.description}</p>
-   <Link to={`/videoGames/${videoGame.id}`}>{videoGame.name}</Link><br /> */}
-   {videoGame.pic_url? (
-     <img src={`${videoGame.pic_url}`} style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
+class VideoGameCard extends Component {
+  
+  render() {
+    const { video_game } = this.props
+
+    return (
+ <div key={video_game.id} className='col-md-3 VideoGameCard text-center rounded'>
+  <Link className='link' to={`/video_games/${video_game.id}`}>{video_game.name}</Link><br />
+   {video_game.pic_url? (
+     <img className='image' style={{height: '250px', width: '250px', margin: 'auto'}} src={`${video_game.pic_url}`} alt={video_game.name} />
    ) : (
      <img src=''
-     style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
+      style={{height: '250px', width: '250px', margin: 'auto'}} alt={video_game.name}/>
    )
   }
- </div><br />
+
  </div>
+    )
+  }
+}
+
 
 export default VideoGameCard
