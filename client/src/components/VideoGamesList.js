449b1ce5e5cfb4367b94949241db43ec2c7d32c8
Pignataro67 <Pignataro67@gmail.com>
2018-11-13 19:40:40 -0800

    fixing typo

--- client/src/components/VideoGamesList.js
+++ client/src/components/VideoGamesList.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import VideoGameCard from './VideoGameCard'
 
-const VideoGamesList = ({ videoGames }) => {
-  const renderVideoGames = videoGames.map((videoGame, i) =>
-    <VideoGameCard key={i} videoGame={videoGame} />
+const VideoGamesList = ({ video_games }) => {
+  const renderVideoGames = video_games.map((video_game, i) =>
+    <VideoGameCard key={i} video_game={video_game} />
   )
 
   return (
-      <div>
+      <div className='container-fluid'>
+        <div className='row'>
         {renderVideoGames}
+        </div>
       </div>
   )
 }
