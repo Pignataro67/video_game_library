d2bdba5b4d0b6e9a553151c59ae5948e665fa472
Pignataro67 <Pignataro67@gmail.com>
2018-11-13 19:37:47 -0800

    adding file with better aesthetics

--- client/src/components/VideoGameShowUI.js
+++ client/src/components/VideoGameShowUI.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const VideoGameShowUI = ({ videoGame, handleDelete, toggleEdit }) => 
+const VideoGameShowUI = ({ video_game, handleDelete, }) => 
 
 <div className='container text center'>
-         <h3>Name: {videoGame.name}</h3>
-         {videoGame.pic_url? (
-     <img src={`${videoGame.pic_url}`} style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
+         <h3>Name: {video_game.name}</h3>
+         {video_game.pic_url? (
+     <img src={`${video_game.pic_url}`} style={{height: '250px', width: '250px', margin: 'auto'}} alt={video_game.name} />
    ) : (
      <img src=''
-     style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
+     style={{height: '250px', width: '250px', margin: 'auto'}} alt={video_game.name} />
    )
   }
-  <p>Description: {videogame.description}</p>
-  <p>Age Range: {videoGame.age_range}</p>
-  <button onClick={() => handleDelete(videoGame)}>Remove Video Game</button><br></br>
-  <button><Link to={`/videoGames/${videoGame.id}/edit`}>Edit</Link><br /></button>
+  <p>Description: {video_game.description}</p>
+  <p>Age Range: {video_game.age_range}</p>
+  <p><button className='btn btn-primary' onClick={() => handleDelete(video_game)}>Remove Video Game</button></p>
+  <p><Link className='btn btn-primary' to={`/video_games/${video_game.id}/edit`}>Edit</Link></p>
 </div>
 
-export default VideoGameUI
+export default VideoGameShowUI
