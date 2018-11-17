845b946cc94ed8d6df21e08bced1081a017b0d7c
Pignataro67 <Pignataro67@gmail.com>
2018-11-17 08:58:51 -0800

    adding default pic

--- client/src/components/VideoGameCard.js
+++ client/src/components/VideoGameCard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
 
 class VideoGameCard extends Component {
   
@@ -13,7 +13,7 @@ class VideoGameCard extends Component {
    {video_game.pic_url? (
      <img className='image' style={{height: '250px', width: '250px', margin: 'auto'}} src={`${video_game.pic_url}`} alt={video_game.name} />
    ) : (
-     <img src=''
+     <img src='x.kinja-static.com/assets/images/logos/placeholders/default.png'
       style={{height: '250px', width: '250px', margin: 'auto'}} alt={video_game.name}/>
    )
   }
@@ -25,3 +25,5 @@ class VideoGameCard extends Component {
 
 
 export default VideoGameCard
+
+// https:'x.kinja-static.com/assets/images/logos/placeholders/default.png'
