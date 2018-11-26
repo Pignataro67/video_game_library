43495c4a399b5fcbce5cc1add87e19cd8c6e6ee3
Pignataro67 <Pignataro67@gmail.com>
2018-11-25 18:09:18 -0800

    adding file with typo correction

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux'
 import videoGamesReducer from './video_games_reducer'
 
 export default combineReducers({
-    video_games: videoGamesReducer
+    videoGames: videoGamesReducer
 })
