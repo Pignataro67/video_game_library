a7e3e35739aff2a420ea398e14146d2367dbf224
Pignataro67 <Pignataro67@gmail.com>
2018-11-25 18:05:20 -0800

    adding file with typo correction

--- client/src/components/VideoGameShowUI.js
+++ client/src/components/VideoGameShowUI.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const VideoGameShowUI = ({ video_game, handleDelete, }) => 
+const VideoGameShowUI = ({ videoGame, handleDelete, }) => 
 
 <div className='container text center'>
-         <h3>Name: {video_game.name}</h3>
-         {video_game.pic_url? (
-     <img src={`${video_game.pic_url}`} style={{height: '250px', width: '250px', margin: 'auto'}} alt={video_game.name} />
+         <h3>Name: {videoGame.name}</h3>
+         {videoGame.pic_url? (
+     <img src={`${videoGame.pic_url}`} style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
    ) : (
      <img src='x.kinja-static.com/assets/images/logos/placeholders/default.png'
-     style={{height: '250px', width: '250px', margin: 'auto'}} alt={video_game.name} />
+     style={{height: '250px', width: '250px', margin: 'auto'}} alt={videoGame.name} />
    )
   }
-  <p>Description: {video_game.description}</p>
-  <p>Age Range: {video_game.age_range}</p>
-  <p><button className='btn btn-primary' onClick={() => handleDelete(video_game)}>Remove Video Game</button></p>
-  <p><Link className='btn btn-primary' to={`/video_games/${video_game.id}/edit`}>Edit</Link></p>
+  <p>Description: {videoGame.description}</p>
+  <p>Age Range: {videoGame.age_range}</p>
+  <p><button className='btn btn-primary' onClick={() => handleDelete(videoGame)}>Remove Video Game</button></p>
+  <p><Link className='btn btn-primary' to={`/video_games/${videoGame.id}/edit`}>Edit</Link></p>
 </div>
 
 export default VideoGameShowUI
