c11338da949dfecb514e01cc2c508dd87ee4794e
Pignataro67 <Pignataro67@gmail.com>
2018-11-27 10:27:26 -0800

    adding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  VideoGame.create(name: 'Contra', age_range: '10-12', pic_url: 'https://i.imgur.com/gyKwGfFb.png')
+  VideoGame.create(name: 'SMB3', age_range: '10-12', pic_url: 'https://i.imgur.com/AMb7VAib.png')
+  VideoGame.create(name: 'Super C', age_range: '10-12', pic_url: 'https://i.imgur.com/dQfocgBb.png')
+  VideoGame.create(name: 'Zelda', age_range: '10-12', pic_url: 'https://i.imgur.com/g4TZnXeb.png')
+  VideoGame.create(name: 'Twisted Metal', age_range: '10-12', pic_url: 'https://i.imgur.com/DwJ2YAob.png')
+  VideoGame.create(name: 'Destiny', age_range: '10-12', pic_url: 'https://i.imgur.com/eZlrelib.jpg')
+  VideoGame.create(name: 'Castlevania', age_range: '10-12', pic_url: 'https://i.imgur.com/3Nbnlu7b.png')    
+              
+#   const videoGames = [
+# //     //   {"id":1,"name":"Contra","age_range":"10-12","pic_url":"https://i.imgur.com/gyKwGfFb.png"},
+# //     //   {"id":2,"name":"Super Mario Bros. 3","age_range":"10-12","pic_url":"https://i.imgur.com/AMb7VAib.png"},
+# //     //   {"id":3,"name":"Zelda","age_range":"10-12","pic_url":"https://i.imgur.com/g4TZnXeb.png"},
+# //     //   {"id":4,"name":"Castlevania","age_range":"10-12","pic_url":"https://i.imgur.com/3Nbnlu7b.png"},
+# //     //   {"id":5,"name":"Super C","age_range":"10-12","pic_url":"https://i.imgur.com/dQfocgBb.png"},
+# //     //   {"id":6,"name":"Twisted Metal","age_range":"10-12","pic_url":"https://i.imgur.com/DwJ2YAob.png"},
+# //     //   {"id":7,"name":"Destiny","age_range":"10-12","pic_url":"https://i.imgur.com/eZlrelib.jpg"}
+# //     //   ]
