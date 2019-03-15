f8405a4bda84cf482159553153c5d9d01eb9fa14
howardbdev <howarddevennish@gmail.com>
2019-03-15 17:29:11 -0400

    remove package-lock, update index.js with compose

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './containers/App';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+// ADD COMPOSE
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 // import * as serviceWorker from './serviceWorker';
@@ -15,14 +16,22 @@ import { Provider } from 'react-redux';
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+// NEW CODE
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, 
-window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__(),
-applyMiddleware(thunk),
+const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk)),
 );
+
+// OLD CODE
+// const store = createStore(rootReducer,
+// window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__(),
+// applyMiddleware(thunk),
+// );
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>, 
+  </Provider>,
   document.getElementById('root'));
