2f05eb5d435ca0176943e1b5593a84c657230588
Pignataro67 <Pignataro67@gmail.com>
2019-07-26 16:36:47 -0700

    refactoring and security update

--- client/src/components/VideoGames.js
+++ client/src/components/VideoGames.js
@@ -9,17 +9,8 @@ class VideoGames extends Component {
     actions.fetchVideoGames()
   }
 
-// export default class VideoGames extends Component {
   render() {
-    // const videoGames = [
-    //   {"id":1,"name":"Contra","age_range":"10-12","pic_url":"https://i.imgur.com/gyKwGfFb.png"},
-    //   {"id":2,"name":"Super Mario Bros. 3","age_range":"10-12","pic_url":"https://i.imgur.com/AMb7VAib.png"},
-    //   {"id":3,"name":"Zelda","age_range":"10-12","pic_url":"https://i.imgur.com/g4TZnXeb.png"},
-    //   {"id":4,"name":"Castlevania","age_range":"10-12","pic_url":"https://i.imgur.com/3Nbnlu7b.png"},
-    //   {"id":5,"name":"Super C","age_range":"10-12","pic_url":"https://i.imgur.com/dQfocgBb.png"},
-    //   {"id":6,"name":"Twisted Metal","age_range":"10-12","pic_url":"https://i.imgur.com/DwJ2YAob.png"},
-    //   {"id":7,"name":"Destiny","age_range":"10-12","pic_url":"https://i.imgur.com/eZlrelib.jpg"}
-    //   ]
+ 
 
     const { videoGames } = this.props
     console.log(videoGames)
