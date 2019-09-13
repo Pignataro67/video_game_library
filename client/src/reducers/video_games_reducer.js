fef866ac05a8888e25d8ff7ee407cbd6d5d480d6
Pignataro67 <Pignataro67@gmail.com>
2019-09-13 08:41:35 -0700

    html practice, spacing improvement

--- client/src/reducers/video_games_reducer.js
+++ client/src/reducers/video_games_reducer.js
@@ -1,28 +1,28 @@
 export default (state = [], action) => {
-    switch (action.type) {
-        case 'FETCH_VIDEO_GAMES':
-          return action.videoGames
-        case 'VIDEO_GAME_FETCHED':
-          const index = state.findIndex(videoGame => videoGame.id === action.videoGame.id)
-          if (index > -1) {
-            return state.map(videoGame => {
-              if (videoGame.id === action.videoGame.id) {
-                return action.videoGame
-              } else {
-                return videoGame
-              }
-            })
-          } else {
-            return [...state, action.videoGame]
-          }
-
-        case 'ADD_VIDEO_GAME':
+  switch (action.type) {
+    case 'FETCH_VIDEO_GAMES':
+      return action.videoGames
+    case 'VIDEO_GAME_FETCHED':
+      const index = state.findIndex(videoGame => videoGame.id === action.videoGame.id)
+        if (index > -1) {
+          return state.map(videoGame => {
+            if (videoGame.id === action.videoGame.id) {
+              return action.videoGame
+            } else {
+              return videoGame
+            }
+          })
+        } else {
           return [...state, action.videoGame]
-        case 'UPDATE_VIDEO_GAME':
-          return state.map(videoGame => videoGame.id === action.videoGame.id ? action.videoGame : videoGame)  
-        case 'DELETE_VIDEO_GAME':
-          return state.filter(videoGame => videoGame.id !== action.id)
-        default:
-          return state
-    }
+        }
+
+    case 'ADD_VIDEO_GAME':
+      return [...state, action.videoGame]
+    case 'UPDATE_VIDEO_GAME':
+      return state.map(videoGame => videoGame.id === action.videoGame.id ? action.videoGame : videoGame)  
+    case 'DELETE_VIDEO_GAME':
+      return state.filter(videoGame => videoGame.id !== action.id)
+    default:
+      return state
+  }
 }
